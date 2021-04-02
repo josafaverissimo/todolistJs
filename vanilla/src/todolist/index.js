ee9f77f7a456c639ccd443a8d284da68680d96c3
Josafá Veríssimo <josafaverissimo98@gmail.com>
2021-04-02 00:05:22 -0300

    feat(todolist): dados persistidos

--- vanilla/src/todolist/index.js
+++ vanilla/src/todolist/index.js
@@ -1,9 +1,22 @@
+function done(element) {
+  const doneContainer = document.querySelector('.addTask__done');
+
+  element.querySelectorAll('button').forEach(function (button) {
+    console.log(button)
+    removeIt(button)
+  })
+
+  doneContainer.appendChild(element.cloneNode(true))
+
+  removeIt(element);
+}
+
 function removeIt(element) {
   element.remove()
 }
 
 function addTask() {
-  const tasks = [];
+  let tasks = [];
 
   function handlerAddTask(event) {
     event.preventDefault()
@@ -12,9 +25,11 @@ function addTask() {
     const tasksWrapper = document.querySelector('.tasksWrapper')
 
     function getTaskData(formDataEntries) {
-      const task = {};
+      const task = {
+        id: Date.now() + Math.ceil(Math.random() * 100)
+      };
 
-      for(let field of formDataEntries) {
+      for (let field of formDataEntries) {
         const fieldName = field[0];
         const value = field[1];
 
@@ -28,38 +43,100 @@ function addTask() {
 
     tasks.push(taskData)
 
-
     tasksWrapper.innerHTML = ''
 
     tasks.forEach(task => {
       addTaskCard(task)
     })
+
+    localStorage.setItem('todoItemsTasks', JSON.stringify(tasks));
+
   }
 
-  function addTaskCard({title, date, time}) {
+  function addTaskCard({ id, title, date, time }) {
+    const cardWrapper = document.createElement('div');
     const card = document.createElement('div');
+    const tasksWrapper = document.querySelector('.tasksWrapper')
+    const cardFooter = document.createElement('div');
+    const buttonDone = document.createElement('button');
+    const buttonRemove = document.createElement('button');
+
+    cardWrapper.appendChild(card)
+
+    cardWrapper.dataset.id = id;
+
+    cardWrapper.classList.add('tasksWrapper__taskCardWrapper')
+
     card.classList.add('tasksWrapper__task');
 
     card.addEventListener('click', event => {
-      removeIt(event.target)
+      done(event.target)
     })
-    
+
     card.innerHTML = `
       <div class="taskWrapper__task__content">
         <h1 class="tasksWrapper__task--title">${title}</h1>
         <h2 class="tasksWrapper__task--subtitle">${date} - ${time}</h2>
       </div>
     `
-    const tasksWrapper = document.querySelector('.tasksWrapper')
 
-    tasksWrapper.appendChild(card);    
+    buttonDone.setAttribute('type', 'button');
+    buttonDone.onclick = () => {
+      removeTask(buttonDone.closest('.tasksWrapper__taskCardWrapper').dataset.id)
+      done(buttonDone.closest('.tasksWrapper__taskCardWrapper'))
+    }
+
+    buttonDone.textContent = 'Feito';
+
+    buttonRemove.setAttribute('type', 'button');
+    buttonRemove.onclick = () => {
+      removeTask(buttonRemove.closest('.tasksWrapper__taskCardWrapper').dataset.id)
+      removeIt(document.querySelector(`[data-id="${id}"]`))
+    }
+    buttonRemove.textContent = 'Remover';
+
+    cardFooter.appendChild(buttonDone)
+    cardFooter.appendChild(buttonRemove)
+
+    cardWrapper.appendChild(cardFooter)
+
+    tasksWrapper.appendChild(cardWrapper);
   }
 
-  return handlerAddTask
+  function removeTask(id) {
+    const newTasks = tasks.reduce((acc, item) => {
+      if (item.id != id) {
+        acc.push(item)
+      }
+
+      return acc
+    }, [])
+
+    tasks = newTasks;
+  }
+
+  function loadTasks(data) {
+    if (data) {
+      tasks = data
+
+      tasks.forEach(task => {
+        addTaskCard(task)
+      })
+    }
+  }
+
+  return {
+    handler: handlerAddTask,
+    loadTasks
+  }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.querySelector('.addTask__form');
+  const task = addTask();
+  const tasksData = JSON.parse(localStorage.getItem('todoItemsTasks'))
 
-  form.addEventListener('submit', addTask())
+  form.addEventListener('submit', task.handler)
+
+  task.loadTasks(tasksData)
 })
